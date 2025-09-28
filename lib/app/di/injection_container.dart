2ce27156fc9f7ac619be7fcac853565571246811
LuisMarioCruzHernandez182 <20221045@uthh.edu.mx>
2025-09-28 16:25:24 -0600

    Cambios en el ijection_container

--- lib/app/di/injection_container.dart
+++ lib/app/di/injection_container.dart
@@ -5,6 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../../core/network/http_client.dart';
 import '../../core/constants/app_constants.dart';
 
+// Auth
 import '../../features/auth/data/datasources/auth_local_datasource.dart';
 import '../../features/auth/data/datasources/auth_remote_datasource.dart';
 import '../../features/auth/data/repositories/auth_repository_impl.dart';
@@ -14,15 +15,27 @@ import '../../features/auth/domain/usecases/logout_usecase.dart';
 import '../../features/auth/domain/usecases/get_current_user_usecase.dart';
 import '../../features/auth/presentation/bloc/auth_bloc.dart';
 
+// Stats
 import '../../features/stats/data/datasources/stats_remote_datasource.dart';
 import '../../features/stats/data/repositories/stats_repository_impl.dart';
 import '../../features/stats/domain/repositories/stats_repository.dart';
 import '../../features/stats/domain/usecases/get_current_month_stats_usecase.dart';
 import '../../features/stats/presentation/bloc/stats_bloc.dart';
 
+import '../../features/inventory/data/datasources/inventory_remote_datasource.dart';
+import '../../features/inventory/data/repositories/inventory_repository_impl.dart';
+import '../../features/inventory/domain/repositories/inventory_repository.dart';
+import '../../features/inventory/domain/usecases/get_inventory_usecase.dart';
+import '../../features/inventory/domain/usecases/get_inventory_stats_usecase.dart';
+import '../../features/inventory/domain/usecases/get_product_by_id_usecase.dart';
+import '../../features/inventory/domain/usecases/get_product_variants_usecase.dart';
+import '../../features/inventory/domain/usecases/update_variant_stock_usecase.dart';
+import '../../features/inventory/presentation/bloc/inventory_bloc.dart';
+
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  // Core
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
   sl.registerLazySingleton<http.Client>(() => http.Client());
@@ -38,6 +51,9 @@ Future<void> init() async {
     ),
   );
 
+  // ============================
+  // Auth
+  // ============================
   sl.registerLazySingleton<AuthRemoteDataSource>(
     () => AuthRemoteDataSourceImpl(
       client: sl<HttpClient>(),
@@ -70,6 +86,9 @@ Future<void> init() async {
     ),
   );
 
+  // ============================
+  // Stats
+  // ============================
   sl.registerLazySingleton<StatsRemoteDataSource>(
     () => StatsRemoteDataSourceImpl(
       client: sl<HttpClient>(),
@@ -88,6 +107,48 @@ Future<void> init() async {
   sl.registerFactory<StatsBloc>(
     () => StatsBloc(
       getCurrentMonthStatsUseCase: sl<GetCurrentMonthStatsUseCase>(),
+    ),
+  );
+
+  // ============================
+  // Inventory
+  // ============================
+  sl.registerLazySingleton<InventoryRemoteDataSource>(
+    () => InventoryRemoteDataSourceImpl(
+      client: sl<HttpClient>(),
+      baseUrl: AppConstants.fullApiUrl,
+    ),
+  );
+
+  sl.registerLazySingleton<InventoryRepository>(
+    () => InventoryRepositoryImpl(
+      remoteDataSource: sl<InventoryRemoteDataSource>(),
+    ),
+  );
+
+  sl.registerLazySingleton<GetInventoryUseCase>(
+    () => GetInventoryUseCase(sl<InventoryRepository>()),
+  );
+  sl.registerLazySingleton<GetInventoryStatsUseCase>(
+    () => GetInventoryStatsUseCase(sl<InventoryRepository>()),
+  );
+  sl.registerLazySingleton<GetProductByIdUseCase>(
+    () => GetProductByIdUseCase(sl<InventoryRepository>()),
+  );
+  sl.registerLazySingleton<GetProductVariantsUseCase>(
+    () => GetProductVariantsUseCase(sl<InventoryRepository>()),
+  );
+  sl.registerLazySingleton<UpdateVariantStockUseCase>(
+    () => UpdateVariantStockUseCase(sl<InventoryRepository>()),
+  );
+
+  sl.registerFactory<InventoryBloc>(
+    () => InventoryBloc(
+      getInventoryUseCase: sl<GetInventoryUseCase>(),
+      getInventoryStatsUseCase: sl<GetInventoryStatsUseCase>(),
+      getProductByIdUseCase: sl<GetProductByIdUseCase>(),
+      getProductVariantsUseCase: sl<GetProductVariantsUseCase>(),
+      updateVariantStockUseCase: sl<UpdateVariantStockUseCase>(),
     ),
   );
 }
